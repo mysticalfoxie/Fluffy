17f604fb89ad670e26daeb7aa823316d25e5ac88
MysticalFoxie <michellebecker2003@gmail.com>
2023-11-10 20:26:55 +0100

    ok last commit for today FFS!

--- Fluffy/Handlers/NsfwHandler.cs
+++ Fluffy/Handlers/NsfwHandler.cs
@@ -50,7 +50,7 @@ public class NsfwHandler : IHandler
             else
             {
                 if (!user.RoleIds.Contains(Program.GuildConfig.NaughtyFoxUnapprovedRoleId))
-                    await user.RemoveRoleAsync(Program.GuildConfig.NaughtyFoxUnapprovedRoleId);
+                    await user.AddRoleAsync(Program.GuildConfig.NaughtyFoxUnapprovedRoleId);
                 
                 await arguments.RespondAsync(
                     ephemeral: true,
